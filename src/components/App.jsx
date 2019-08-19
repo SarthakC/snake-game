797ffe4599c286e683af18e2665af60f4e1ba800
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-19 16:04:37 +0530

    move snake function uncommented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -50,32 +50,32 @@ export default class App extends Component {
     }
   };
 
-  // moveSnake = () => {
-  //   let snakePulse = this.state.pulse;
-  //   let snakeDots = [...this.state.snakeBodyCoordinates];
-  //   let snakeHead = snakeDots[snakeDots.length - 1];
-  //   switch (this.state.direction) {
-  //     case "RIGHT":
-  //       snakeHead = [snakeHead[0] + snakePulse, snakeHead[1]];
-  //       break;
-  //     case "LEFT":
-  //       snakeHead = [snakeHead[0] - snakePulse, snakeHead[1]];
-  //       break;
-  //     case "UP":
-  //       snakeHead = [snakeHead[0], snakeHead[1] - snakePulse];
-  //       break;
-  //     case "DOWN":
-  //       snakeHead = [snakeHead[0], snakeHead[1] + snakePulse];
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //   snakeDots.push(snakeHead);
-  //   snakeDots.shift();
-  //   this.setState({
-  //     snakeBodyCoordinates: snakeDots
-  //   });
-  // };
+  moveSnake = () => {
+    let snakePulse = this.state.pulse;
+    let snakeDots = [...this.state.snakeBodyCoordinates];
+    let snakeHead = snakeDots[snakeDots.length - 1];
+    switch (this.state.direction) {
+      case "RIGHT":
+        snakeHead = [snakeHead[0] + snakePulse, snakeHead[1]];
+        break;
+      case "LEFT":
+        snakeHead = [snakeHead[0] - snakePulse, snakeHead[1]];
+        break;
+      case "UP":
+        snakeHead = [snakeHead[0], snakeHead[1] - snakePulse];
+        break;
+      case "DOWN":
+        snakeHead = [snakeHead[0], snakeHead[1] + snakePulse];
+        break;
+      default:
+        break;
+    }
+    snakeDots.push(snakeHead);
+    snakeDots.shift();
+    this.setState({
+      snakeBodyCoordinates: snakeDots
+    });
+  };
 
   checkBorderCollision() {
     let snakeHead = this.state.snakeBodyCoordinates[
