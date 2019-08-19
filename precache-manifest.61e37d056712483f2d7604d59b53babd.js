e807fbabae2aaaa4bf57c17a302b7d26a2f2673b
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-19 15:54:10 +0530

    Updates

--- precache-manifest.61e37d056712483f2d7604d59b53babd.js
+++ precache-manifest.61e37d056712483f2d7604d59b53babd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fbcf7700de2adf3b99702aad97c8a8e8",
+    "revision": "8c9d41326c5160ffc478ad20a52baf9c",
     "url": "/snake-game/index.html"
   },
   {
-    "revision": "6bdd21a5f9bf1c3d0fbc",
+    "revision": "2779dbabbe4a127a38d1",
     "url": "/snake-game/static/css/main.9f7a634c.chunk.css"
   },
   {
-    "revision": "8dacc5662720dd383c27",
-    "url": "/snake-game/static/js/2.3c6c2d7d.chunk.js"
+    "revision": "0952524e81a609b472a1",
+    "url": "/snake-game/static/js/2.417f5f80.chunk.js"
   },
   {
-    "revision": "6bdd21a5f9bf1c3d0fbc",
-    "url": "/snake-game/static/js/main.2d45ae38.chunk.js"
+    "revision": "2779dbabbe4a127a38d1",
+    "url": "/snake-game/static/js/main.58af214c.chunk.js"
   },
   {
     "revision": "e07b58a07e60bf3e4cae",
