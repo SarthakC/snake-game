50e3e53088bf129da41c6bab523084418cfc51ea
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-19 16:01:12 +0530

    reverted commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import GoogleAuth from "./GoogleAuth";
 
 const Header = props => {
   return (
-    <div className="ui menu">
-      <div className="header item">{props.title}</div>
-      <div className="item">{props.score}</div>
-      <div className="item">{props.prevHighScore}</div>
-      <div className="right menu">
-        <GoogleAuth />
-      </div>
+    <div class="ui menu">
+      <div class="header item">{props.title}</div>
+      <div class="item">{props.score}</div>
+      <div class="item">{props.prevHighScore}</div>
     </div>
   );
 };
